2aa62bb38d9dd190f5757a50c64438fbe4a8b702
PixelSyntax <amitthapa181133@gmail.com>
2025-12-03 11:39:23 +0530

    Update FundMeTest.t.sol

--- test/FundMeTest.t.sol
+++ test/FundMeTest.t.sol
@@ -11,17 +11,14 @@ contract FundMeTest is Test {
     uint256 constant STARTING_BALANCE = 10 ether;
 
     function setUp() public {
-        // Deploy FundMe using Sepolia ETH/USD price feed
         fundMe = new FundMe(0x694AA1769357215DE4FAC081bf1f309aDC325306);
         vm.deal(USER, STARTING_BALANCE);
     }
 
-    // ✅ Test: minimum USD should be 5
     function testMinimumUsdIsFive() public view {
         assertEq(fundMe.MIN_USD(), 5e18);
     }
 
-    // ✅ Test: owner should be deployer
     function testOwnerIsMsgSender() public view {
         assertEq(fundMe.i_owner(), address(this));
     }
